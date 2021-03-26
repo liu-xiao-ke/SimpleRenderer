5e6029d08597b22adb96c221fcf45cc895d99c18
lkhappy <18310901312@163.com>
2021-03-26 19:27:52 +0800

    add sphere bounding box

--- src/shape/sphere.cpp
+++ src/shape/sphere.cpp
@@ -3,6 +3,13 @@
 //
 
 #include "sphere.h"
-namespace sr{
+namespace sr {
 
+    Bounds3f Sphere::ObjectBound() const {
+        Point3f pMin(-radius, -radius, zMin), pMax(radius, radius, zMax);
+        if (phiMax <= 3 * Pi / 2) {
+            pMin = Point3f(radius * std::cos(phiMax), radius * std::sin(phiMax), zMin);
+        }
+        return Bounds3f(pMin, pMax);
+    }
 }
--- src/shape/sphere.h
+++ src/shape/sphere.h
@@ -10,9 +10,20 @@
 namespace sr{
     class Sphere : public Shape {
     public:
+        Sphere(const Transform *ObjectToWorld, const Transform *WorldToObject, bool reverseOrientation, Float radius,
+               Float zMin, Float zMax, Float phiMax) : Shape(ObjectToWorld, WorldToObject, reverseOrientation),
+                                                       radius(radius),
+                                                       zMin(Clamp(std::min(zMin, zMax), -radius, radius)),
+                                                       zMax(Clamp(std::max(zMin, zMax), -radius, radius)),
+                                                       phiMax(Radians(Clamp(phiMax, 0, 360))),
+                                                       thetaMin(std::acos(Clamp(zMin / radius, -1, 1))),
+                                                       thetaMax(std::acos(Clamp(zMax / radius, -1, 1))) {}
+        Bounds3f ObjectBound() const override;
 
     private:
-
+        const Float radius;
+        const Float zMin, zMax;
+        const Float thetaMin, thetaMax, phiMax;
     };
 }
 
